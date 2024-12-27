83cd09c0cde884d06698d5242eabbfcd44feb9c9
Kuge Riku <k01rain6h126o@gmail.com>
2024-12-27 15:08:48 +0900

    12月27日変更

--- app5.js
+++ app5.js
@@ -100,5 +100,33 @@ app.get("/gacha", (req, res) => {
   res.render('gacha', { number: num, gacha: gacha });
 });
 
+app.get("/selcoin", (req, res) => {
+  const select = req.query.select === '0' ? 'オモテ' : 'ウラ';
+  let correct = Number(req.query.correct) || 0;
+  let total = Number(req.query.total) || 0;
+  console.log({ select, correct, total });
+
+  const num = Math.floor(Math.random() * 2); // 0 or 1
+  const cpu = num === 0 ? 'オモテ' : 'ウラ';
+
+  let judgement = '';
+  if (select === cpu) {
+    judgement = 'アタリ';
+    correct += 1;
+  } else {
+    judgement = 'ハズレ';
+  }
+  total += 1;
+
+  const display = {
+    your: select,
+    cpu: cpu,
+    judgement: judgement,
+    correct: correct,
+    total: total
+  };
+
+  res.render('selcoin', display);
+});
 
 app.listen(8080, () => console.log("Example app listening on port 8080!"));
